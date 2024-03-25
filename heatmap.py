02941ce9ad0911a7760fb4354837f87f871244b6
lcernyo <72266252+lcernyo@users.noreply.github.com>
2024-03-25 12:00:39 -0700

    fixed missing data folder issue

---
+++ heatmap.py
@@ -0,0 +1,299 @@
+import time, os, glob
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms
+
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+import copy
+from torch.utils.data import Dataset
+
+import argparse
+
+
+def th_delete(tensor, indices):
+    mask = torch.ones(tensor.numel(), dtype=th.bool)
+    mask[indices] = False
+    return tensor[mask]
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Heatmap generation parameters")
+
+    # Define arguments with their default values
+    parser.add_argument("--model", type=str, default='r18', help="Choose model: r18, swinv2, or wrn50.")
+    parser.add_argument("--model_path", type=str, default='./r18.pt', help="Saved model")
+    parser.add_argument("--smaller_model_path", type=str, default='./r18_small.pt', help="If multiple FOV, specify path to second saved model")
+    parser.add_argument("--wsi_dir", type=str, default='inf_tiles/', help="Directory to retrieve the patches for heatmaps")
+    parser.add_argument("--save_np_dir", type=str, default='./Infseg/', help="Where to save the numpy heatmap")
+
+    parser.add_argument("--multiFOV", type=int, default=0, help="0 for single FOV, 1 for multiFOV")
+    parser.add_argument("--bs", type=int, default=16, help="Batch size")
+    parser.add_argument("--workers", type=int, default=16, help="Number of workers")
+    parser.add_argument("--stride", type=int, default=64, help="Stride for heatmap generation")
+    parser.add_argument("--img_size", type=int, default=3072, help="Tile size from wsi_dir")
+    parser.add_argument("--gpu", type=int, default=0, help="Which GPU to use")
+
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Now you can use the arguments as variables in your code
+    print(f"Architecture for training: {args.model}")
+    print(f"WSI Tile Directory: {args.wsi_dir}")
+    print(f"Path to saved model : {args.model_path}")
+    print(f"Path to saved heatmaps : {args.save_np_dir}")
+
+    if args.multiFOV == 1:
+        print("MultiFOV selected")
+        print(f"Path to smaller FOV saved model : {args.smaller_model_path}")
+    
+    print(f"Batch size: {args.bs}")
+    print(f"Stride: {args.stride}")
+    print(f"Tile size: {args.img_size}")
+    print(f"Workers: {args.workers}")
+    print(f"GPU: {args.gpu}")
+    
+
+    IMG_DIR  = args.wsi_dir
+    MODEL_SEG_DIR = args.model_path
+    MODEL_SEG_DIR_SMALL = args.smaller_model_path
+    SAVE_NP_DIR = args.save_np_dir
+    isMultiFOV = args.multiFOV
+    
+    if not os.path.exists(SAVE_NP_DIR):
+        print("Heatmap folder you provided us does not exist, being created now...")
+        os.makedirs(SAVE_NP_DIR)
+
+
+    MODEL = args.model
+    patch_size = args.img_size
+    tile_size = int(args.img_size / 6)
+    stride = args.stride
+    batch_size = args.bs 
+    num_workers = args.workers
+
+    class HeatmapDataset(Dataset):
+        def __init__(self, tile_dir, row, col, stride=1):
+            """
+            Args:
+                tile_dir (string): path to the folder where tiles are
+                row (int): row index of the tile being operated
+                col (int): column index of the tile being operated
+                stride: stride of sliding 
+            """
+            self.tile_size = tile_size
+            self.img_size = patch_size
+            self.stride = stride
+            padding = tile_size
+            large_img = torch.ones(3, 3*self.img_size, 3*self.img_size)
+
+            for i in [-1,0,1]:
+                for j in [-1,0,1]:
+                    img_path = tile_dir+'/'+str(row+i)+'/'+str(col+j)+'.jpg'
+                    try:
+                        img = Image.open(img_path)
+                        img = transforms.ToTensor()(img) 
+                    except:
+                        img = torch.ones(3,self.img_size, self.img_size)
+
+                    large_img[:, (i+1)*self.img_size:(i+2)*self.img_size,(j+1)*self.img_size:(j+2)*self.img_size] = img
+
+            large_img = normalize(large_img)
+
+            self.padding_img = large_img[:,self.img_size-padding:2*self.img_size+padding, self.img_size-padding:2*self.img_size+padding]
+            self.len = (self.img_size//self.stride)**2
+
+        def __getitem__(self, index):
+
+            row = (index*self.stride // self.img_size)*self.stride
+            col = (index*self.stride % self.img_size)
+
+            img = self.padding_img[:, row:row+self.tile_size, col:col+self.tile_size]        
+
+            return img
+
+        def __len__(self):
+            return self.len
+
+    normalize = transforms.Normalize(mean=[0.5962484, 0.5533902, 0.6344057],
+                             std=[0.00056322647, 0.00075884577, 0.00043305202])
+
+    to_tensor = transforms.ToTensor()
+
+    # Retrieve Files
+    filenames = glob.glob(IMG_DIR + '*')
+    filenames = [filename.split('/')[-1] for filename in filenames]
+    filenames = sorted(filenames,reverse=False)
+    print("Cases to be processed: ", filenames)
+
+
+    # Check GPU:
+    use_gpu = torch.cuda.is_available()
+
+    if MODEL == 'wrn50':
+        seg_model = torchvision.models.wide_resnet50_2(pretrained =True)
+        seg_model.fc = nn.Linear(2048, 3) 
+    elif MODEL == 'swinv2':
+        seg_model = torchvision.models.swin_v2_t(weights='IMAGENET1K_V1')
+        seg_model.head = nn.Linear(768,3)
+    elif MODEL == 'r18':
+        seg_model = torchvision.models.resnet18(pretrained =True)
+        seg_model.fc = nn.Linear(512, 3)
+    else:
+        print("Model name not recognized, using resnet18")
+        seg_model = torchvision.models.resnet18(pretrained =True)
+        seg_model.fc = nn.Linear(512, 3) 
+
+    checkpoint = torch.load(MODEL_SEG_DIR,map_location=torch.device('cuda:'+str(args.gpu)))
+
+    if isMultiFOV:
+        seg_model_small = seg_model
+        checkpoint_small = torch.load(MODEL_SEG_DIR_SMALL,map_location=torch.device('cuda:'+str(args.gpu)))
+        seg_model_small.load_state_dict(checkpoint_small['model_state_dict'])
+
+    seg_model.load_state_dict(checkpoint['model_state_dict'])
+
+    if use_gpu:
+        seg_model = seg_model.cuda(args.gpu)
+        if isMultiFOV:
+            seg_model_small = seg_model_small.cuda(args.gpu)
+    else:
+        print("Look into possible GPU issues") 
+
+
+    for filename in filenames[:]:
+        print("Now processing: ", filename)
+
+        # Retrieve Files
+        TILE_DIR = IMG_DIR+'{}/0/'.format(filename)
+
+        imgs = []
+        for target in sorted(os.listdir(TILE_DIR)):
+            d = os.path.join(TILE_DIR, target)
+            if not os.path.isdir(d):
+                continue
+
+            for root, _, fnames in sorted(os.walk(d)):
+                for fname in sorted(fnames):
+                    if fname.endswith('.jpg'):
+                        path = os.path.join(root, fname)
+                        imgs.append(path)
+
+        rows = [int(image.split('/')[-2]) for image in imgs]
+        row_nums = max(rows) + 1
+        #cols = [int(image.split('/')[-1].split('.')[0]) for image in imgs]
+        cols = [int(image.split('/')[-1].split('.')[0].split('_')[-1]) for image in imgs]
+        col_nums = max(cols) +1    
+        
+        print("Row nums = ", row_nums)
+        print("Col nums = ", col_nums)
+
+        # Initialize outputs accordingly:
+        heatmap_res = patch_size // stride
+        
+        print("heatmap resolution = ", heatmap_res*row_nums, " x ", heatmap_res*col_nums)
+        seg_output = np.zeros((heatmap_res*row_nums, heatmap_res*col_nums,3), dtype=float)
+        
+        
+        seg_model.train(False)
+        if isMultiFOV:
+            seg_model_small.train(False)
+
+        start_time = time.perf_counter() 
+
+        for row in tqdm(range(row_nums)):
+            for col in range(col_nums):
+
+                image_datasets = HeatmapDataset(TILE_DIR, row, col, stride=stride)
+                dataloader = torch.utils.data.DataLoader(image_datasets, batch_size=batch_size,
+                                                     shuffle=False, num_workers=num_workers)
+
+                # For Stride 32 (BrainSeg):
+                running_seg = torch.zeros((args.stride), dtype=torch.uint8)
+                output_class = np.zeros((heatmap_res, heatmap_res,3), dtype=float)
+
+                for idx, data in enumerate(dataloader):
+                    # get the inputs
+                    inputs = data
+                    # wrap them in Variable
+                    if use_gpu:
+                        with torch.no_grad():
+
+
+                            inputs = Variable(inputs.cuda(args.gpu))
+
+                            predict = seg_model(inputs)
+                            _, indices = torch.max(predict.data, 1) # indices = 0:Background, 1:WM, 2:GM
+                            indices = indices.type(torch.uint8)
+                            running_seg =  indices.data.cpu()
+
+                            softmaxxxed = F.softmax(predict)
+
+                            softmaxxxed = softmaxxxed.cpu().numpy()
+
+                            if isMultiFOV == 1:
+                                if 2 in running_seg[idx_conf]:                                
+                                    # Assuming you have img tensor already defined
+                                    batch_size, channels, height, width = inputs.shape
+
+                                    # Top-left corner
+                                    img1 = inputs[:, :, 0:height//2, 0:width//2]
+
+                                    # Top-right corner
+                                    img2 = inputs[:, :, 0:height//2, width//2:width]
+
+                                    # Bottom-left corner
+                                    img3 = inputs[:, :, height//2:height, 0:width//2]
+
+                                    # Bottom-right corner
+                                    img4 = inputs[:, :, height//2:height, width//2:width]
+
+                                    listy = [softmaxxxed]
+                                    for img in [img1,img2,img3,img4]:
+                                        predict = seg_model_small(img)
+
+                                        _, indices = torch.max(predict.data, 1) # indices = 0:Background, 1:WM, 2:GM  
+                                        softmaxxxed_small = F.softmax(predict)
+
+                                        softmaxxxed_small = softmaxxxed_small.cpu().numpy()
+
+                                        listy.append(softmaxxxed_small)
+                                else:
+                                    listy = [softmaxxxed,softmaxxxed,softmaxxxed,softmaxxxed,softmaxxxed]
+
+                                stacked_array = np.stack(tuple(listy), axis=0)
+
+                                # For Stride 32 (BrainSeg) :
+                                i = (idx // (heatmap_res//batch_size))
+                                j = (idx % (heatmap_res//batch_size))
+                                transposed_array = np.transpose(stacked_array, (1, 2, 0))
+                                output_class[i,j*batch_size:(j+1)*batch_size] = transposed_array
+
+                            else:
+
+                                # For Stride 32 (BrainSeg) :
+                                i = (idx // (heatmap_res//batch_size))
+                                j = (idx % (heatmap_res//batch_size))
+                                output_class[i,j*batch_size:(j+1)*batch_size,:] = softmaxxxed
+
+                # Final Outputs of Brain Segmentation
+                seg_output[row*heatmap_res:(row+1)*heatmap_res, col*heatmap_res:(col+1)*heatmap_res] = output_class
+
+        np.save(SAVE_NP_DIR+filename, seg_output)
+
+        # Time Statistics for Inference
+        end_time = time.perf_counter()
+        print("Time to process " \
+              + filename \
+              + ": ", end_time-start_time, "sec")
+
+
+if __name__ == "__main__":
+    main()
---
+++ training.py
@@ -0,0 +1,404 @@
+import pickle
+import torch
+import torchvision
+import torch.nn as nn
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader, ConcatDataset, Sampler
+from torchvision import transforms, utils
+from PIL import Image
+
+from sklearn.metrics import classification_report, plot_confusion_matrix, balanced_accuracy_score
+
+import numpy as np
+import statistics
+import argparse
+import os
+
+class Patches(Dataset):
+    """Non-infarcted WSI dataset"""
+
+    def __init__(self, path, cases, transform=None):
+        """
+        Args:
+            path (string): Path to the tile folder directory.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.path = path
+        self.cases = cases
+        self.WSI = os.listdir(path)
+        
+        self.im_paths, self.gt = self.grabTiles()
+        self.targets = self.gt
+        
+        self.transform = transform
+        
+    def grabTiles(self):
+        im_paths = []
+        gt = []
+        
+        inf_ct = 0
+        heal_ct = 0
+        bg_ct = 0
+        
+        for wsi in self.WSI:
+            heal_ct_case = 0
+            bg_ct_case = 0
+            if wsi in self.cases:
+                for cls in os.listdir(self.path+wsi):
+                    for image in os.listdir(self.path+wsi+'/'+cls):
+                        if cls == 'BG':# and bg_ct_case <= 300:
+                            gt.append(0)
+                            bg_ct += 1
+                            bg_ct_case += 1
+                            
+                            im_path = self.path+wsi+'/'+cls+'/'+image
+                            im_paths.append(im_path)
+                        elif cls == 'Heal':# and heal_ct_case <= 300:
+                            gt.append(1)
+                            heal_ct += 1
+                            heal_ct_case += 1
+                            
+                            im_path = self.path+wsi+'/'+cls+'/'+image
+                            im_paths.append(im_path)
+                        elif cls == 'Inf':
+                            gt.append(2)
+                            inf_ct += 1
+                            
+                            im_path = self.path+wsi+'/'+cls+'/'+image
+                            im_paths.append(im_path)
+                            
+        print("Total number of tiles: ")
+        print("Heal = ", heal_ct, "| Inf = ", inf_ct, "| BG = ", bg_ct)
+        
+        return im_paths, gt
+
+    def __len__(self):
+        return len(self.im_paths)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = self.im_paths[idx]
+        cls = self.gt[idx]
+        
+        #image = io.imread(img_name)
+        image = Image.open(img_name)
+        
+        if self.transform:
+            image = self.transform(image)
+            
+        sample = [image, cls]
+
+        return sample
+    
+class BalancedBatchSampler(Sampler):
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.class_counts = self._get_class_counts()
+        self.min_class_count = min(self.class_counts.values())
+        self.indices_by_class = self._indices_by_class()
+
+    def _get_class_counts(self):
+        target = self.dataset.targets if hasattr(self.dataset, "targets") else self.dataset.labels
+        return {class_id: sum(1 for label in target if label == class_id) for class_id in set(target)}
+
+    def _indices_by_class(self):
+        target = self.dataset.targets if hasattr(self.dataset, "targets") else self.dataset.labels
+        return {class_id: [i for i, label in enumerate(target) if label == class_id] for class_id in set(target)}
+
+    def __iter__(self):
+        for _ in range(self.min_class_count):
+            for class_id, indices in self.indices_by_class.items():
+                yield indices.pop(torch.randint(0, len(indices), size=(1,)).item())
+
+    def __len__(self):
+        return self.min_class_count * len(self.class_counts)   
+    
+def randomInit(m):
+    print("Model Randomly Initialized")
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        torch.nn.init.zeros_(m.bias)
+        
+
+def fit(MODEL,LR,model, loss_fn, optimizer, train_loader,train_loader_eval, val_loader, num_epochs, scheduler = None, stat_count=100, gpu=0,PATH = './saved_models/resnet18_Inf.pt'):
+    curr_model_score = -1
+    loss_epoch = []
+    losses = []
+    train_epoch = []
+    val_epoch = []
+    
+    f1_val_epoch = []
+    f1_train_epoch = []
+    bacc_val_epoch = []
+    bacc_train_epoch = []
+
+    
+    device = torch.device('cuda:'+str(gpu) if torch.cuda.is_available() else 'cpu')
+    model.to(gpu)
+
+    randomInit(model)
+
+    if scheduler == None:
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)    
+    
+    num_steps = len(train_loader)
+    
+    # Iterate through all Epochs
+    for epoch in range(num_epochs):
+        if epoch != 0:
+            scheduler.step()
+            loss_epoch.append(statistics.mean(losses))
+            train_epoch.append((total_train_correct/total_train))
+            val_epoch.append((total_correct/total_val))
+            losses = []
+            
+        for train_ct in range(num_steps):           
+            try:
+                data = next(labelled_iter)
+            except:
+                labelled_iter = iter(train_loader)
+                data = next(labelled_iter)
+
+            with torch.enable_grad():
+                model.train()
+                images, labels = data[0].to(device,dtype=torch.float), data[1].to(device,dtype=torch.long)
+                
+                #print(labels)
+                
+                optimizer.zero_grad()
+                outputs = model(images)
+                
+                loss = loss_fn(outputs, labels)
+                
+                loss.backward()
+                losses.append(loss.item())
+                optimizer.step()
+                # Print statistics on every stat_count iteration
+                if (train_ct+1) % stat_count == 0:
+                    print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                                %(epoch+1, num_epochs, train_ct+1, 
+                                len(train_loader), loss.item()))
+                
+        with torch.no_grad():
+            model.eval()
+            
+            total_val = 0
+            total_correct = 0
+            val_predicted_full = []
+            val_labels_full = []
+            for data in val_loader:
+                images, labels = data[0].cuda(gpu), data[1].cuda(gpu)
+                outputs = model(images)
+
+                _, val_predicted = torch.max(outputs.data, 1)
+                total_val += labels.size(0)
+                total_correct += (val_predicted == labels).sum().item()
+
+                val_predicted_full = val_predicted_full + val_predicted.cpu().data.numpy().tolist()
+                val_labels_full = val_labels_full + labels.cpu().data.numpy().tolist()
+                
+                
+            total_train = 0
+            total_train_correct = 0
+            train_predicted_full = []
+            train_labels_full = []
+            for data in train_loader_eval:
+                images, labels = data[0].cuda(gpu), data[1].cuda(gpu)
+                outputs = model(images)
+
+                _, train_predicted = torch.max(outputs.data, 1)
+                total_train += labels.size(0)
+                total_train_correct += (train_predicted == labels).sum().item()
+
+                train_predicted_full = train_predicted_full + train_predicted.cpu().data.numpy().tolist()
+                train_labels_full = train_labels_full + labels.cpu().data.numpy().tolist()
+                
+                
+        print("END OF EPOCH")
+        val_dict = classification_report(val_labels_full, val_predicted_full, labels=[0,1,2],output_dict=True)
+        train_dict = classification_report(train_labels_full, train_predicted_full, labels=[0,1,2],output_dict=True)
+        
+        bacc_val = balanced_accuracy_score(val_labels_full,val_predicted_full)
+        bacc_train = balanced_accuracy_score(train_labels_full,train_predicted_full)
+
+        val0_score = val_dict['0']['f1-score'] + 0.0001
+        val1_score = val_dict['1']['f1-score'] + 0.0001
+        val2_score = val_dict['2']['f1-score'] + 0.0001
+        
+        train0_score = train_dict['0']['f1-score'] + 0.0001
+        train1_score = train_dict['1']['f1-score'] + 0.0001
+        train2_score = train_dict['2']['f1-score'] + 0.0001
+        
+        f1_val_epoch.append(val_dict['2']['f1-score'])
+        f1_train_epoch.append(train_dict['2']['f1-score'])
+        
+        bacc_val_epoch.append(bacc_val)
+        bacc_train_epoch.append(bacc_train)
+        
+        print("Validation Set: ")
+        print("Model Score = ", (total_correct/total_val)*val0_score*val1_score*val2_score)
+        print("Accuracy = ", (total_correct/total_val))
+        print("bAccuracy = ", (bacc_val))
+        print("inf F1 = ", (val_dict['2']['f1-score']))
+        
+        print("Train Set: ")
+        print("Model Score = ", (total_train_correct/total_train)*train0_score*train1_score*train2_score)
+        print("Accuracy = ", (total_train_correct/total_train))
+        print("bAccuracy = ", (bacc_train))
+        print("inf F1 = ", (train_dict['2']['f1-score']))
+        
+
+        if curr_model_score < bacc_val: 
+            curr_model_score = bacc_val
+            print("Model Checkpoint saved!")
+            torch.save({'model_state_dict': model.state_dict()}, PATH)
+        
+    try:
+        plt.plot(loss_epoch,label='Loss')
+        plt.savefig(MODEL+'_lr'+LR+'_'+'loss_plot.png')
+
+        plt.plot(train_epoch,label='Train Acc')
+        plt.plot(val_epoch,label='Val Acc')
+        plt.savefig(MODEL+'_lr'+LR+'_'+'acc_plot.png')
+        
+        plt.plot(bacc_train_epoch,label='Train bAcc')
+        plt.plot(bacc_val_epoch,label='Val bAcc')
+        plt.savefig(MODEL+'_lr'+LR+'_'+'bacc_plot.png')
+        
+        plt.plot(bacc_train_epoch,label='Train Infarct F1')
+        plt.plot(f1_val_epoch,label='Val Infarct F1')
+        plt.savefig(MODEL+'_lr'+LR+'_'+'f1_plot.png')
+    except:
+        pass
+
+    return loss_epoch, train_epoch, val_epoch
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Training parameters")
+
+    # Define arguments with their default values
+    parser.add_argument("--model", type=str, default='r18', help="Choose model: r18, swinv2, or wrn50.")
+    parser.add_argument("--segmentation_tile_dir", type=str, default='seg_data/', help="Directory for patched images")
+    parser.add_argument("--save_path", type=str, default='./r18.pt', help="Where to save the model")
+    parser.add_argument("--gpu", type=int, default=0, help="GPU to be used for BG segmentation")
+
+    parser.add_argument("--bs", type=int, default=5, help="Batch size")
+    parser.add_argument("--ep", type=int, default=40, help="Epochs")
+    parser.add_argument("--lr", type=int, default=0.01, help="Learning rate")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Now you can use the arguments as variables in your code
+    print(f"Architecture for training: {args.model}")
+    print(f"GPU: {args.gpu}")
+    print(f"Segmentation Tile Directory: {args.segmentation_tile_dir}")
+    print(f"Path to saved model after training: {args.save_path}")
+    print(f"Batch size: {args.bs}")
+    print(f"Epochs: {args.ep}")
+    print(f"Learning rate: {args.lr}")
+
+    MODEL = args.model
+    SEGMENTATION_TILE_DIR = args.segmentation_tile_dir
+    gpu = args.gpu
+    SAVE_PATH = args.save_path
+    EPOCH = args.ep
+    BS = args.bs
+    LR = args.lr
+
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5962484, 0.5533902, 0.6344057],
+                             std=[0.00056322647, 0.00075884577, 0.00043305202])
+    ])
+
+
+    test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5962484, 0.5533902, 0.6344057],
+                                 std=[0.00056322647, 0.00075884577, 0.00043305202])
+        ])
+
+    all_cases = os.listdir(SEGMENTATION_TILE_DIR)
+
+    train_cases = []
+    val_cases = ['NA5137-16_HE','NA5137-17_HE','NA5137-18_HE','NA5057-16_HE','NA5057-17_HE','NA5057-18_HE','NA5077-16_HE','NA5077-17_HE','NA5077-18_HE']
+    
+    #val_cases = ['NA5031-18_HE']
+
+    test_cases = ['NA5090-16_HE','NA5090-17_HE','NA5090-18_HE','NA5051-16_HE','NA5051-17_HE','NA5051-18_HE','NA5146-16_HE','NA5146-17_HE','NA5146-18_HE','NA5063-16_HE','NA5063-17_HE','NA5063-18_HE','NA5089-16_HE','NA5089-17_HE','NA5089-18_HE']
+
+    delete_cases = ['NA5116-16_HE','NA5116-17_HE','NA5116-18_HE']
+    
+    #for testing
+#     val_cases = ['NA5093-17_HE']
+#     test_cases = ['NA5093-17_HE']
+
+
+    all_cases = list(set(all_cases) - set(delete_cases))
+
+
+    train_cases = list(set(all_cases) - set(test_cases))
+    train_cases = list(set(train_cases) - set(val_cases))
+
+    print("Working on train set")
+    trainset = Patches(SEGMENTATION_TILE_DIR,train_cases,train_transform)
+
+    print("Working on validation set")
+    valset = Patches(SEGMENTATION_TILE_DIR,val_cases,test_transform)
+
+
+    batch_size = BS
+    num_epochs = EPOCH
+
+    loss_f = nn.CrossEntropyLoss()
+
+
+    if MODEL == 'wrn50':
+        model = torchvision.models.wide_resnet50_2(pretrained =True)
+        model.fc = nn.Linear(2048, 3) 
+    elif MODEL == 'swinv2':
+        model = torchvision.models.swin_v2_t(weights='IMAGENET1K_V1')
+        model.head = nn.Linear(768,3)
+    elif MODEL == 'r18':
+        model = torchvision.models.resnet18(pretrained =True)
+        model.fc = nn.Linear(512, 3)
+    else:
+        print("Model name not recognized, using resnet18")
+        model = torchvision.models.resnet18(pretrained =True)
+        model.fc = nn.Linear(512, 3) 
+
+    optimizer = torch.optim.Adam(model.parameters(),lr = 0.01)
+
+    target = trainset.targets
+
+    class_sample_count = np.array(
+        [len(np.where(target == t)[0]) for t in np.unique(target)])
+    weight = 1. / class_sample_count
+    samples_weight = np.array([weight[t] for t in target])
+
+
+    sampler = torch.utils.data.sampler.WeightedRandomSampler(
+                    weights=samples_weight,
+                    num_samples=len(trainset), replacement=True)
+
+
+    train_loader = DataLoader(trainset, batch_size=batch_size, sampler=sampler)
+
+    train_loader_eval = train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(valset,batch_size=batch_size,shuffle=True)
+
+    loss_epoch, train_epoch, val_epoch = fit(MODEL,LR,model, loss_f, optimizer, train_loader,train_loader_eval, val_loader, num_epochs,gpu=gpu,PATH = SAVE_PATH)
+
+
+if __name__ == "__main__":
+    main()
